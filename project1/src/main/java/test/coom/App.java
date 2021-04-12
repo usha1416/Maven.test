6f7567ca27328a0a53f7cb66aa774a2f3e249a12
usha <yusha1416@gmail.com>
2021-04-12 15:53:00 +0530

    project1

---
+++ project1/src/main/java/test/coom/App.java
@@ -0,0 +1,13 @@
+package test.coom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
